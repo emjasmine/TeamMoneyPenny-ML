ad8bb0ecf3a3862a34cec66ba590e161b4d49677
eglulu6 <eglulu6@gmail.com>
2021-06-03 19:32:51 -0400

    saving progress

--- TMPMachineLearning/static/js/sample.js
+++ TMPMachineLearning/static/js/sample.js
@@ -11,23 +11,3 @@
 tickerUrl = '/tickers';
 d3.json(tickerUrl).then(function (data)
 { console.log(data) })
-
-//         // select ticker dropdown
-//         var dropdown = d3.select("#istock");
-//         data.lvl1.levl2.forEach((d) => 
-//         {
-//             // Append a tr to the table body tag
-//             var row = dropdown.append('option');
-//             var row = tbody.append("tr");
-//             Object.values(d).forEach((element, index) => 
-//             {
-//                 if (index <= 5) 
-//                 {
-//                     row.append("td").text(element);
-//                 }
-//             });
-//         });
-//     });
-}
-
-
