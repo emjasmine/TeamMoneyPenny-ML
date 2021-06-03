845ccde2d33f10f2df9eb1ec273c76e5c9bedbd7
emjasmine <75378436+emjasmine@users.noreply.github.com>
2021-06-02 21:56:48 -0400

    test js

--- TMPMachineLearning/static/js/sample.js
+++ TMPMachineLearning/static/js/sample.js
@@ -8,26 +8,9 @@
 ****************************************************************/
 
 // call BERTs_Flask for ticker funtion results
-tickerUrl = '/tickers';
-d3.json(tickerUrl).then(function (data)
-{ console.log(data) })
-
-//         // select ticker dropdown
-//         var dropdown = d3.select("#istock");
-//         data.lvl1.levl2.forEach((d) => 
-//         {
-//             // Append a tr to the table body tag
-//             var row = dropdown.append('option');
-//             var row = tbody.append("tr");
-//             Object.values(d).forEach((element, index) => 
-//             {
-//                 if (index <= 5) 
-//                 {
-//                     row.append("td").text(element);
-//                 }
-//             });
-//         });
-//     });
+function tickerTable(){
+    tickerUrl = '/tickers';
+    d3.json(tickerUrl).then(function (data)
+    { console.log(data) })
 }
-
-
+tickerTable();
