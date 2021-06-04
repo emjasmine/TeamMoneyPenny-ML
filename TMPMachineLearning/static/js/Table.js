136fe2eaa0fcd92cc51edaf94621d98faaf15dce
eglulu6 <eglulu6@gmail.com>
2021-06-04 15:06:48 -0400

    updating form post with all fields captured

--- TMPMachineLearning/static/js/Table.js
+++ TMPMachineLearning/static/js/Table.js
@@ -13,15 +13,24 @@ d3.select("#submit").on("click", feedBert)
 function feedBert()
 {
     let stock = d3.select("#istock").node().value
-    // let industry = d3.select("#industry")
-    // let price = d3.select("#price")
-    // let low_risk = d3.select("#low_risk")
-    // let moderate_risk = d3.select("#moderate_risk")
-    // let high_risk = d3.select("#high_risk")
+    let industry = d3.select("#industry")
+    let price = d3.select("#price")
+    let low_risk = d3.select("#low_risk")
+    let moderate_risk = d3.select("#moderate_risk")
+    let high_risk = d3.select("#high_risk")
+    console.log('form submission { istock: ${stock}, industry: ${industry},price: ${price},low_risk: ${low_risk},moderate_risk: ${moderate_risk},high_risk: ${high_risk} }')
     d3.json('/ticker_select', 
     {
         method: 'POST',
-        body: JSON.stringify({'istock': stock})
+        body: JSON.stringify(
+        {
+            'istock': stock,
+            'industry': industry,
+            'price': price,
+            'low_risk': low_risk,
+            'moderate_risk': moderate_risk,
+            'high_risk': high_risk,
+        })
     }).then(data => console.log(data))
 }
 
@@ -35,27 +44,27 @@ function table(){
 }
 table();
 
-function stocksTable(anyData) 
-{
-    // refrence the table body to build table in 
-    var tbody = d3.select('.stock-table');
+// function stocksTable(anyData) 
+// {
+//     // refrence the table body to build table in 
+//     var tbody = d3.select('.stock-table');
     
-    // Use for each to create a table with ALL the data
-    anyData.map(stock => 
-    {
-        // Ticker,Company,Industry,Price,Dividends,ROI,Risk,
-        //Append a tr to the table body tag
-        var row = tbody.append('tr');
-        // Append td.text  for each info column
-        // tkr symbol col
-        row.append("td").text(stock.symbol);
-        // company name
-        row.append("td").text(stock.Name);
-        // stock price
-        row.append("td").text(price);
-        // dividend rate
-        row.append("td").text(`$${divAmount}`);
-        // rate of return
-        row.append("td").text(`${ROI(price, divAmount)}`);
-    });
-}
+//     // Use for each to create a table with ALL the data
+//     anyData.map(stock => 
+//     {
+//         // Ticker,Company,Industry,Price,Dividends,ROI,Risk,
+//         //Append a tr to the table body tag
+//         var row = tbody.append('tr');
+//         // Append td.text  for each info column
+//         // tkr symbol col
+//         row.append("td").text(stock.symbol);
+//         // company name
+//         row.append("td").text(stock.Name);
+//         // stock price
+//         row.append("td").text(price);
+//         // dividend rate
+//         row.append("td").text(`$${divAmount}`);
+//         // rate of return
+//         row.append("td").text(`${ROI(price, divAmount)}`);
+//     });
+// }
