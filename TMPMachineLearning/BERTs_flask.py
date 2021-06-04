5cfa12c43ae3e02a0005b07f6b679d5a18cd561e
pramodphilip <75338454+pramodphilip@users.noreply.github.com>
2021-06-03 23:02:29 -0400

    addition of /ticker_select to BERTS_flask.py

--- TMPMachineLearning/BERTs_flask.py
+++ TMPMachineLearning/BERTs_flask.py
@@ -10,7 +10,7 @@ import json
 import matplotlib.pyplot as plt
 from statistics import mode
 from datetime import datetime
-from flask import render_template, jsonify
+from flask import render_template, request
 from . import app
 
 
@@ -19,19 +19,44 @@ def tickers():
     ########## Read in data file ##########
     path = 'TMPMachineLearning/static/resources/Flask_table.csv'
     Flask_table = pd.read_csv(path,index_col = False)
-    # ########## Pull only the Ticker coulmns ##########
-    # Ticker = Flask_table['Ticker']
     Flask_table = Flask_table.to_json(orient='records', indent=4)
     return Flask_table
 
-# @app.route('/industry')
-# def industry():
-#     ########## Read in data file ##########
-#     path = 'TMPMachineLearning/static/resources/Flask_table.csv'
-#     Flask_table = pd.read_csv(path,index_col = False)
-#     ########## Pull only the Industry coulmns ##########
-#     Industry = Flask_table['Industry']
-#     Industry = Industry.to_json(orient='records', indent=4)
-#     return jsonify(Industry)
+@app.route('/ticker_select', methods=['GET','POST'])
+def select():
+
+    ########## Read in data file ##########
+    path = 'static/resources/Flask_table.csv'
+    flask_data = pd.read_csv(path)
+    flask_data = flask_data.drop("Unnamed: 0", axis=1)
+
+    ########## Grab stock input fields ##########
+    if request.method == 'GET':
+        form_data = {"istock": "none",
+                    "industry": "none",
+                    "price": "none",
+                    "low_risk": "none",
+                    "moderate_risk": "none",
+                    "high_risk": "none"}
+        return form_data
+
+    elif request.method == 'POST':
+        data = request.form
+        print(data)
+
+    select_stock = flask_data.loc[flask_data['Ticker'] == data['istock']]
+    stock_cluster = select_stock['Clusters'].index.values.tolist()
+
+    # Finds tickers in the same cluster as the selected stock
+    cluster_data = flask_data.loc[flask_data['Clusters'] == stock_cluster[0]]
+
+    cluster = cluster_data.to_json(orient='records',indent=4)
+    print("Please work, please")
+    return cluster
+
+
+    
+
+
 
 
