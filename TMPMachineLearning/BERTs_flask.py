ddc68d1878eb93e5087ce154c55384bb767a8184
eglulu6 <eglulu6@gmail.com>
2021-06-03 22:36:31 -0400

    populating dropdowns

--- TMPMachineLearning/BERTs_flask.py
+++ TMPMachineLearning/BERTs_flask.py
@@ -10,32 +10,28 @@ import json
 import matplotlib.pyplot as plt
 from statistics import mode
 from datetime import datetime
-from flask import render_template
+from flask import render_template, jsonify
 from . import app
 
 
-@app.route('/tickers')
+@app.route('/Flask_table')
 def tickers():
-    
     ########## Read in data file ##########
     path = 'TMPMachineLearning/static/resources/Flask_table.csv'
     Flask_table = pd.read_csv(path,index_col = False)
-    ########## Pull only the Ticker coulmns ##########
-    Ticker = Flask_table['Ticker']
-    result = Ticker.to_json(orient='records', indent=4)
-    # print(f"rendering {Ticker}")
-    # print(Flask_table)
-    # return
-    return result
+    # ########## Pull only the Ticker coulmns ##########
+    # Ticker = Flask_table['Ticker']
+    Flask_table = Flask_table.to_json(orient='records', indent=4)
+    return Flask_table
 
-@app.route('/industry')
-def industry():
-    ########## Read in data file ##########
-    path = 'static/resources/Flask_table.csv'
-    Flask_table = pd.read_csv(path,index_col = False)
-    ########## Pull only the Industry coulmns ##########
-    Industry = Flask_table['Industry']
-    Industry = json.dumps(Industry, indent=4)
-    return Industry
+# @app.route('/industry')
+# def industry():
+#     ########## Read in data file ##########
+#     path = 'TMPMachineLearning/static/resources/Flask_table.csv'
+#     Flask_table = pd.read_csv(path,index_col = False)
+#     ########## Pull only the Industry coulmns ##########
+#     Industry = Flask_table['Industry']
+#     Industry = Industry.to_json(orient='records', indent=4)
+#     return jsonify(Industry)
 
 
---
+++ TMPMachineLearning/static/js/Form.js
@@ -0,0 +1,35 @@
+/****************************************************************
+            Declare variables for use in functions
+****************************************************************/
+Url = '/Flask_table';
+d3.json(Url).then(stockdata => 
+{ 
+    getTickers(stockdata);
+    getIndustry(stockdata);
+    /****************************************************************
+                            Fetch all tickers
+    ****************************************************************/
+    function getTickers(anyData)
+    {
+        console.log('Function: getTickers')  
+        let tickerList = anyData.map(data => 
+            d3.select('#istock')
+            .append('option')
+            .text(data.Ticker))
+    }
+    /****************************************************************
+                Fetch all industries from BERTs Flask app
+    ****************************************************************/
+    function getIndustry(anyData)
+    {
+        console.log('Function: getIndustry')  
+        let tickerList = anyData.map(data => 
+            d3.select('#industry')
+            .append('option')
+            .text(data.Industry))
+    }
+    /****************************************************************
+                    Call Form Tag Functions
+    ****************************************************************/
+}
+        
